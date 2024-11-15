ef9868bce711e7037ad55e9c8937048ef91117d4
Nekylaw <nekylaw.ao@gmail.com>
2024-11-15 15:47:34 +0100

    Update Card.cs

--- Assets/_Scripts/Card.cs
+++ Assets/_Scripts/Card.cs
@@ -9,82 +9,83 @@ public class Card : MonoBehaviour
     public CardScriptable card = new CardScriptable();
     public MonumentScriptable monument;
 
-    List<Card> unlockedMonuments = new List<Card>(); //Liste où y aura tous les monuments débloqués par le joueur / ia
+    List<Card> unlockedMonuments = new List<Card>(); 
     
 
 
-    //La fonction qui gère les effets des cartes hors monuments
-
-    //CardPlayed correspond à la carte joué pendant le tour actuel (est définit dans la classe player)
-    public void Effect(player cardPlayed, string playerSendingEffect, string playerReceivingEffect, int moneyCurrentlayer)
-    {
+ 
+    //public void Effect(player cardPlayed, string playerSendingEffect, string playerReceivingEffect, int moneyCurrentlayer)
+    //{
 
         
-        if (cardPlayed.color == "Bleu")
-        {
+    //    if (cardPlayed.color == "Bleu")
+    //    {
 
-
-
-            playerSendingEffect.money += cardPlayed.gainValue;
-            playerReceivingEffect.money += cardPlayed.gainValue;
+    //        playerSendingEffect.money += cardPlayed.gainValue;
+    //        playerReceivingEffect.money += cardPlayed.gainValue;
 
             
-        }
-        if (cardPlayed.color == "Rouge")
-        {
-            playerReceivingEffect.money += cardPlayed.gainValue;
-        }
-        if (cardPlayed.color == "Vert")
-        {
-            if (cardPlayed.name)
-                playerSendingEffect.money += cardPlayed.gainValue;
-        }
-        if (cardPlayed.color == "Violet")
-        {
+    //    }
+    //    if (cardPlayed.color == "Rouge")
+    //    {
+    //        playerReceivingEffect.money += cardPlayed.gainValue;
+    //    }
+    //    if (cardPlayed.color == "Vert")
+    //    {
+    //        if (cardPlayed.name)
+    //            playerSendingEffect.money += cardPlayed.gainValue;
+    //    }
+    //    if (cardPlayed.color == "Violet")
+    //    {
 
-            playerSendingEffect.money += cardPlayed.gainValue;
-            playerReceivingEffect.money -= cardPlayed.gainValue;
-        }
+    //        playerSendingEffect.money += cardPlayed.gainValue;
+    //        playerReceivingEffect.money -= cardPlayed.gainValue;
+    //    }
+
+
+    //}
 
 
 
-    }
-    //Gère les effets des monuments
-    public void monumentEffect()
-    {
 
-        foreach(var monument in unlockedMonuments)
-        {
-            if(monument.name == "Gare")
-            {
-                player.nbDiceAllowed = 2;
-            }
-            if(monument.name == "Tour radio")
-            {
-                player.rerollDice = true;
-            }
-            if(monument.name == "Centre commercial")
-            {
-                foreach(var card in listCardPlayer)
-                {
-                    if(card.type == "Restauration" || card.type == "Magasin")
-                    {
-                        card.gainValue += 1;
-                    }
+
+
+
+    //public void monumentEffect()
+    //{
+
+    //    foreach(var monument in unlockedMonuments)
+    //    {
+    //        if(monument.name == "Gare")
+    //        {
+    //            player.nbDiceAllowed = 2;
+    //        }
+    //        if(monument.name == "Tour radio")
+    //        {
+    //            player.rerollDice = true;
+    //        }
+    //        if(monument.name == "Centre commercial")
+    //        {
+    //            foreach(var card in listCardPlayer)
+    //            {
+    //                if(card.type == "Restauration" || card.type == "Magasin")
+    //                {
+    //                    card.gainValue += 1;
+    //                }
                     
-                }
+    //            }
 
-            }
-            if(monument.name == "Parc")
-            { 
-                if(player.dice1 == PlayerPrefs.dice2)
-                {
+    //        }
+    //        if(monument.name == "Parc")
+    //        { 
+    //            if(player.dice1 == PlayerPrefs.dice2)
+    //            {
 
-                }
-            }
+    //            }
+    //        }
 
-        }
-    }
+    //    }
+    
 
 
 
